9abed13a70eaa0a0a4ad47757d814e09265303b0
cifelse <meetcif@gmail.com>
2023-10-19 11:05:45 +0800

    Add Controllers

---
+++ src/controllers/Controller.java
@@ -0,0 +1,14 @@
+package controllers;
+
+public abstract class Controller {
+    protected MainController controller;
+
+    /**
+     * Allows every controller to access the Main Controller by setting their ControllerMain as the main controller.
+     * @param controller the generated ControllerMain in Driver.java
+     */
+    public void setMainController(MainController controller) {
+        this.controller = controller;
+    }
+
+}
---
+++ src/controllers/CreditsController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class CreditsController extends Controller{
+    @FXML
+    public void goBackAction() throws IOException {
+        controller.changeScene(MainController.PAGE_MAIN);
+    }
+}
